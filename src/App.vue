80412f55ccc0ea0f5e9136ac4c6922051acb53c7
BlaD200 <blad600@gmail.com>
2020-10-19 17:16:28 +0300

    Added file uploading.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <header>
+            <b-navbar toggleable="lg" type="dark" variant="dark" fixed="top">
+                <b-navbar-brand class="mx-auto" href="#">Timetable generation algorithm</b-navbar-brand>
+            </b-navbar>
+        </header>
+        <b-container class="pt-4 mt-5">
+            <genetic-algorithm class="mx-auto"></genetic-algorithm>
+        </b-container>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    // import HelloWorld from './components/HelloWorld.vue'
+    import GeneticAlgorithm from "@/components/GeneticAlgorithm";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+        components: {
+            GeneticAlgorithm
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    }
 </style>
---
+++ src/components/GeneticAlgorithm.vue
@@ -0,0 +1,61 @@
+<template>
+    <div>
+        <file-pond
+                id="file-upload"
+                name="file"
+                ref="pond"
+                :allow-multiple="true"
+                :max-files="5"
+                accepted-file-types="application/json"
+                server="http://localhost:9000/load/json/all"
+                v-bind:files="files"
+                v-on:init="handleFilePondInit"
+        />
+    </div>
+</template>
+
+<script>
+    // Import Vue FilePond
+    import vueFilePond from 'vue-filepond';
+
+    // Import FilePond styles
+    import 'filepond/dist/filepond.min.css';
+
+    // Import FilePond plugins
+    // Please note that you need to install these plugins separately
+
+    // Import image preview and file type validation plugins
+    import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type';
+
+    // Create component
+    const FilePond = vueFilePond(FilePondPluginFileValidateType);
+
+    // import LoadFile from "@/components/LoadFile";
+    export default {
+        name: "genetic-algorithm",
+        components: {
+            FilePond
+        },
+        data() {
+            return {
+                files: []
+            }
+        },
+        methods: {
+            handleFilePondInit: function() {
+                console.log('FilePond has initialized');
+
+                // FilePond instance methods are available on `this.$refs.pond`
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        width: 400px;
+    }
+    p {
+        text-align: center;
+    }
+</style>
